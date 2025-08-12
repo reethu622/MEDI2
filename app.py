d841ca3640d52f17d214303fd5cce516ef634a1f
Reeth <80691413+reethu622@users.noreply.github.com>
2025-08-12 08:28:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ def serve_static(path):
     if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
-        return send_from_directory(app.static_folder, "index.html")
+        return send_from_directory(app.static_folder, "medibot.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
